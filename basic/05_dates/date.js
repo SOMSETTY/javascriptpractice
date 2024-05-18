14e9adb3d6b89d3c0c21e6e1495e7c7b26d71f0a
sandeep kumar <somsettysandeep@gmail.com>
2024-05-18 22:27:17 +0530

    dates

---
+++ basic/05_dates/date.js
@@ -0,0 +1,29 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+// console.log(typeof myDate)
+
+
+// let myCreateDate = new Date(2023,0,23)
+// let myCreateDate = new Date(2023,0,23,5,3)
+// let myCreateDate = new Date("2023-03-14")
+let myCreateDate = new Date("1995-01-14");
+//  console.log(myCreateDate.toLocaleString());
+
+// console.log(myCreateDate.toDateString());
+
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());

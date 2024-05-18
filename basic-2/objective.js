311f4f568aa483ace8c3bc8bc2b270d4437d8a1e
sandeep kumar <somsettysandeep@gmail.com>
2024-05-19 02:52:24 +0530

    objective part -1

---
+++ basic-2/objective.js
@@ -0,0 +1,41 @@
+//  singleton
+//Object.create
+
+// object literals
+
+const mysys = Symbol("key1")
+
+
+const jsUser = {
+    name : "hitesh",
+    "full name":"somsetty sandeep kumar",
+    age: 18,
+    [mysys]:"mykey1",
+    location:"hyderabad",
+    email:"somesetty@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays:["monday","tuesday"]
+}
+
+// console.log(jsUser.location) // it isone method of accesing objective.
+
+// console.log(jsUser["email"])
+// console.log(jsUser["age"])
+// console.log(jsUser["full name"])
+// console.log(jsUser[mysys]);
+
+jsUser.email= "s.somsetty@gmail.com";
+// Object.freeze(jsUser)
+jsUser.email="hitesh@microsoft.com"
+console.log(jsUser)
+
+
+jsUser.greeting = function(){
+    console.log("hello js user")
+}
+jsUser.greetingtwo = function(){
+    console.log(`hello js user,${this.name}`)
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingtwo());

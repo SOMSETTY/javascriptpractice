667c7bfc0813781bb57a91b877c8b5886c91ea80
sandeep kumar <somsettysandeep@gmail.com>
2024-05-19 01:02:31 +0530

    array

--- basic-2/array.js
+++ basic-2/array.js
@@ -3,7 +3,45 @@ const myHeors = [ "balaya", "venkatesh","akhil","chiru"]
 
 const myArr2 = new Array (1,2,3,4,5 )
 
-console.log(myArr[3])
-console.log(myHeors[3])
+// console.log(myArr[3])
+// console.log(myHeors[3])
 
 // array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(119));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+
+// console.log(myArr)
+// console.log(typeof(newArr))   /// checked in next class
+
+// slice , spice
+
+console.log ("A",myArr)
+
+const myn1 = myArr.slice(1,3)
+
+ console.log(myn1);
+
+console.log ("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+
+console.log ("c",myArr)
+
+console.log(myn2);
+
+
+
+

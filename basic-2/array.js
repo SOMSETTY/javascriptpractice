b73194f1824c72d8231fc292e0737999ab7f903f
sandeep kumar <somsettysandeep@gmail.com>
2024-05-19 00:24:47 +0530

    changes

--- basic-2/array.js
+++ basic-2/array.js
@@ -1,1 +1,9 @@
-const MYarray
+const myArr = [0,1,2,3,4,5]
+const myHeors = [ "balaya", "venkatesh","akhil","chiru"]
+
+const myArr2 = new Array (1,2,3,4,5 )
+
+console.log(myArr[3])
+console.log(myHeors[3])
+
+// array methods

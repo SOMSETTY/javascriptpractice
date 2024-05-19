bba252a49a3739b388eb1de1cdf13d92191321bc
sandeep kumar <somsettysandeep@gmail.com>
2024-05-19 18:37:13 +0530

    destructing object

---
+++ basic-2/api.js
@@ -0,0 +1,13 @@
+const course = {
+    course : "js in hindi",
+    price : "999",
+    courseInstructor : "hitesh" 
+}
+
+
+// course.courseInstructor
+
+const {courseInstructor} = course
+
+console.log(course)
+console.log(courseInstructor)

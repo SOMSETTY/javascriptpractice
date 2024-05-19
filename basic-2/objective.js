884cb336dfc10ed9cd3b45a38f53691950428512
sandeep kumar <somsettysandeep@gmail.com>
2024-05-19 15:54:46 +0530

    objective part

--- basic-2/objective.js
+++ basic-2/objective.js
@@ -3,19 +3,19 @@
 
 // object literals
 
-const mysys = Symbol("key1")
+// const mysys = Symbol("key1")
 
 
-const jsUser = {
-    name : "hitesh",
-    "full name":"somsetty sandeep kumar",
-    age: 18,
-    [mysys]:"mykey1",
-    location:"hyderabad",
-    email:"somesetty@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays:["monday","tuesday"]
-}
+// const jsUser = {
+//     name : "hitesh",
+//     "full name":"somsetty sandeep kumar",
+//     age: 18,
+//     [mysys]:"mykey1",
+//     location:"hyderabad",
+//     email:"somesetty@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays:["monday","tuesday"]
+// }
 
 // console.log(jsUser.location) // it isone method of accesing objective.
 
@@ -24,18 +24,31 @@ const jsUser = {
 // console.log(jsUser["full name"])
 // console.log(jsUser[mysys]);
 
-jsUser.email= "s.somsetty@gmail.com";
+// jsUser.email= "s.somsetty@gmail.com";
 // Object.freeze(jsUser)
-jsUser.email="hitesh@microsoft.com"
-console.log(jsUser)
+// jsUser.email="hitesh@microsoft.com"
+// console.log(jsUser)
 
 
-jsUser.greeting = function(){
-    console.log("hello js user")
-}
-jsUser.greetingtwo = function(){
-    console.log(`hello js user,${this.name}`)
-}
+// jsUser.greeting = function(){
+//     console.log("hello js user")
+// }
+// jsUser.greetingtwo = function(){
+//     console.log(`hello js user,${this.name}`)
+// }
 
-console.log(jsUser.greeting());
-console.log(jsUser.greetingtwo());
+// console.log(jsUser.greeting());
+// console.log(jsUser.greetingtwo());
+
+//second method of writing object
+const jsUser = {}
+
+     jsUser.id = "hitesh",
+    jsUser.fullname ="somsetty sandeep kumar",
+    jsUser.age= 18,
+    jsUser.location="hyderabad",
+    jsUser.email="somesetty@gmail.com",
+    jsUser.isLoggedIn= false,
+    jsUser.lastLoginDays=["monday","tuesday"]
+
+    console.log(jsUser)

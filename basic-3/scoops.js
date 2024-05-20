5ddd42c7088b6ca1ff5c20ec7fa709bb335e7f8f
sandeep kumar <somsettysandeep@gmail.com>
2024-05-20 23:26:09 +0530

    function

--- basic-3/scoops.js
+++ basic-3/scoops.js
@@ -4,17 +4,53 @@ if (true) {
     let a = 10
     const b = 30
     // var c = 40
-    console.log("INNER:",a);
+    // console.log("INNER:",a);
 }
 
-for( let index=0; index<array.length;index++){
-    const element = array[index];   
-}
+// for( let index=0; index<array.length;index++){
+//     const element = array[index];   
+// }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one (){
+    const username = "hitesh"
+
+    function two (){
+        const website = "youtube"
+        // console.log(username);
+    } 
+    // console.log(website)
+    two()
+}
+one ()
+
+if (true) {
+    const username = "sandeep"
+    if (username==="sandeep"){
+        const website= "   youtube"
+        // console.log (username + website);
+    }
+// console.log(website);
+}
+//  console.log(username);
+
+//*********interesting**********/
+
+console.log(addone(5))
+
+function addone (num){
+ return num + 1
+}
+
+
+console.log(addtwo(5))
+
+const addtwo = function(num){
+    return num + 2
+}
 

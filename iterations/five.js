6884da7dd097fab41027e1220e3154434fece15a
sandeep kumar <somsettysandeep@gmail.com>
2024-06-03 00:34:10 +0530

    for each

---
+++ iterations/five.js
@@ -0,0 +1,45 @@
+const coding = ["js","Ruby","java","python","cpp"]
+
+// coding.forEach(  function (item){
+//     console.log(item)
+// })
+
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item)
+// }
+
+
+// coding.forEach(printMe)
+
+
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// });
+
+const myCoding = [
+    {
+        languageName : "java script",
+        languageFileName : "js"
+    },
+    {
+        languageName : "java ",
+        languageFileName : "java"
+    },
+    {
+        languageName : "paython",
+        languageFileName : "py"
+    }
+]
+
+
+myCoding.forEach ((item,index,arr)=>{
+ console.log(item,index,arr)
+console.log(item.languageName
+
+)
+})
